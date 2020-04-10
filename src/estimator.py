48e0e08de312d083e2dbb159b4bbd13ecbfa5130
catherine244 <catenduku995@gmail.com>
2020-04-10 14:40:26 +0300

    challenges modified

--- src/estimator.py
+++ src/estimator.py
@@ -1,96 +1,69 @@
-def get_days(period_type, time_elapsed):
-    if period_type == 'days':
-        return time_elapsed
+def estimator(data):
+    #initialization of variables used
+    impact = {}
+    severeImpact = {}
+    reportedCases = data['reportedCases']
+    periodType = data['periodType']
+    timeToElapse = data['timeToElapse']
+    totalHospitalBeds = data['totalHospitalBeds']
+    avgDailyIncome = data['region']['avgDailyIncomeInUSD']
+    avgDailyIncomePopulation = data['region']['avgDailyIncomePopulation']
 
-    if period_type == 'weeks':
-        return time_elapsed * 7
+    
+    #CHALLENGE 1
 
-    if period_type == 'months':
-        return time_elapsed * 30
+    duration = duration_to_days(periodType, timeToElapse)
+    factor = duration//3
 
+    #For Impact
+    impact['currentlyInfected'] = reportedCases * 10
+    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * (pow(2,factor))
 
-def get_currently_infected_people(data, output):
-    reported_cases = data['reportedCases']
-    output['impact']['currentlyInfected'] = reported_cases * 10
-    output['severeImpact']['currentlyInfected'] = reported_cases * 50
+    #For Severe Impact
+    severeImpact['currentlyInfected'] = reportedCases * 50
+    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected']*(pow(2,factor))
 
+    #CHALLENGE 2
 
-def get_infections_by_requested_time(data, output):
-    days = get_days(data['periodType'], data['timeToElapse'])
-    output['impact']['infectionsByRequestedTime'] = \
-        int(output['impact']['currentlyInfected'] * (2 ** int(days / 3)))
-
-    output['severeImpact']['infectionsByRequestedTime'] = \
-        int(output['severeImpact']['currentlyInfected'] * (2 ** int(days / 3)))
-
-
-def get_severe_cases_by_requested_time(output):
-    output['impact']['severeCasesByRequestedTime'] = \
-        int(0.15 * output['impact']['infectionsByRequestedTime'])
-
-    output['severeImpact']['severeCasesByRequestedTime'] = \
-        int(0.15 * output['severeImpact']['infectionsByRequestedTime'])
+    #For Impact
+    impact['severeCasesByRequestedTime'] = int(impact['infectionsByRequestedTime'] * 0.15) 
+    impact['hospitalBedsByRequestedTime'] = beds_available(totalHospitalBeds, impact['severeCasesByRequestedTime'])
 
 
-def get_hospital_beds_by_requested_time(data, output):
-    available_beds = 0.35 * data['totalHospitalBeds']
+    #For Severe Impact
+    severeImpact['severeCasesByRequestedTime'] = int(severeImpact['infectionsByRequestedTime'] * 0.15)
+    severeImpact['hospitalBedsByRequestedTime'] = beds_available(totalHospitalBeds, severeImpact['severeCasesByRequestedTime'])
 
-    impact_severe_cases = output['impact']['severeCasesByRequestedTime']
-    output['impact']['hospitalBedsByRequestedTime'] = int(available_beds - impact_severe_cases)
+    #CHALLENGE 3
+    
 
-    s_impact_severe_cases = output['severeImpact']['severeCasesByRequestedTime']
-    output['severeImpact']['hospitalBedsByRequestedTime'] = int(available_beds - s_impact_severe_cases)
+    #For Impact
+    impact['casesForICUByRequestedTime'] = int(impact['infectionsByRequestedTime'] * 0.05)
+    impact['casesForVentilatorsByRequestedTime'] = int(impact['infectionsByRequestedTime'] * 0.02)
+    impact['dollarsInFlight'] =int((impact['infectionsByRequestedTime'] * avgDailyIncomePopulation * avgDailyIncome)/ duration)
 
-
-def get_cases_for_icu_by_requested_time(output):
-    output['impact']['casesForICUByRequestedTime'] = \
-        int(0.05 * output['impact']['infectionsByRequestedTime'])
-
-    output['severeImpact']['casesForICUByRequestedTime'] = \
-        int(0.05 * output['severeImpact']['infectionsByRequestedTime'])
-
-
-def get_cases_for_ventilators_by_requested_time(output):
-    output['impact']['casesForVentilatorsByRequestedTime'] = \
-        int(0.02 * output['impact']['infectionsByRequestedTime'])
-
-    output['severeImpact']['casesForVentilatorsByRequestedTime'] = \
-        int(0.02 * output['severeImpact']['infectionsByRequestedTime'])
+    #For Severe Impact
+    severeImpact['casesForICUByRequestedTime'] =  int(severeImpact['infectionsByRequestedTime'] * 0.05)
+    severeImpact['casesForVentilatorsByRequestedTime'] = int(severeImpact['infectionsByRequestedTime'] * 0.02)
+    severeImpact['dollarsInFlight'] = int((severeImpact['infectionsByRequestedTime'] * avgDailyIncomePopulation * avgDailyIncome) /duration)
 
 
-def get_dollars_in_flight(data, output):
-    population_perc = data['region']['avgDailyIncomePopulation']
-    population_inc = data['region']['avgDailyIncomeInUSD']
-    days = get_days(data['periodType'], data['timeToElapse'])
-
-    impact_infections = output['impact']['infectionsByRequestedTime']
-    output['impact']['dollarsInFlight'] = \
-        int((impact_infections * population_perc * population_inc) / days)
-
-    s_impact_infections = output['severeImpact']['infectionsByRequestedTime']
-    output['severeImpact']['dollarsInFlight'] = \
-        int((s_impact_infections * population_perc * population_inc) / days)
 
 
-def estimation(data):
-    # Assuming 'data' is a dictionary
-
-    output = {
-        'data': data,
-        'impact': {},
-        'severeImpact': {}
-    }
-
-    get_currently_infected_people(data, output)
-    get_infections_by_requested_time(data, output)
-    get_severe_cases_by_requested_time(output)
-    get_hospital_beds_by_requested_time(data, output)
-    get_cases_for_icu_by_requested_time(output)
-    get_cases_for_ventilators_by_requested_time(output)
-    get_dollars_in_flight(data, output)
+    output = {'data':data, 'impact':impact, 'severeImpact': severeImpact}
 
     return output
 
+def duration_to_days(durationType ,time):
+    if durationType == "months":
+        time *= 30
+    elif durationType == "weeks":
+        time *= 7
 
+    return time
 
-
+#calculating the number of beds available
+def beds_available(totalbeds, severecases):
+    available_beds = (0.35 * totalbeds) - severecases
+    #return the available beds as integers and not floats
+    return int(available_beds)
